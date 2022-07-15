69721f40c224ad99a95d70d83efcfcff2d29b69d
Santi Arroyo <santi.arroyo551@gmail.com>
2022-07-15 12:47:13 -0400

    commit tree c10516d95fb9b8a7be2e50a742d3f3c036ad0807

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+/*function hasTargetSum(array, target) {
+  
+  const firstNum = array[0];
+  const lastNum = array[array.length -1]
+  const addNum = firstNum + lastNum
+  
+  if (addNum === target)  return true
+  else {
+    return false 
 }
+}*/
+
+
+
+/*function hasTargetSum (array, target) {
+  for (let i = 0; i < array.length; i++) {
+    //n steps
+    for (let j = i + 1; j < array.length; j++) {
+      //n * n steps
+      if (array[j] === target - array[i]) return true
+    }
+  } return false
+} //runtime: O(n^2) not great
+*/
+
+
+/*function hasTargetSum(array, target){
+  const seenNumbers = {}
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+      if (seenNumbers[complement]) return true; 
+      seenNumbers[array[i]] = true 
+  }
+  return false 
+}*/
+
+function hasTargetSum(array, target){
+  const trackNumbers = {}
+  for (const number of array) {
+    const complement = target - number
+      if (complement in trackNumbers) return true; 
+      trackNumbers[number] = true 
+  }
+  return false 
+}
+
+
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  -write a function that receives 2 arguments - an array of integers and a target intger
+  -the function should return true if any pair of numbers in the array adds up to the target number
+  -if th pair of numbers in the array do NOT add up to the target the function should return false
 */
 
 /*
